654efd6c616af998ad1bee22797dbc2cc801b0a3
QWERT-commits <SimonShi1092@outlook.com>
2022-03-01 19:04:21 +0800

    Update Restart-1.py

--- Restart-1.py
+++ Restart-1.py
@@ -2,30 +2,43 @@
 from random import randint
 from math import sin
 
+def returnmenu():
+    mainprogram()
+    
 def pythagoreantherom(sideselection):
     if sideselection == 1:
         adjacentlength1 = int(input("Type the first adjacent length of the triangle: "))
         adjacentlength2 = int(input("Type the second adjacent length of the triangle: "))
         hypotenuselength1 = pow((pow(adjacentlength1,2) + pow(adjacentlength2,2)),0.5)
         print("This is the length of the hypotenuse:",hypotenuselength1)
+        print('Execution completed, returning to homemenu... \n')
+        returnmenu()
     if sideselection == 2: 
         hypotenuselength1 = int(input("Type the length of the hypotenuse: "))
-        adjece2 = int(input("Type the length of the other hypotenuse: "))
+        adjacentlength2 = int(input("Type the length of the other hypotenuse: "))
+        hypotenuselength1 = pow((pow(hypotenuselength1,2) - pow(adjacentlength2)),0.5)
+        print("This is the length of the other adjacent side:",hypotenuselength1)
+        print('Execution completed, returning to homemenu... \n')
+        returnmenu()
         
-    
-list1 = ['Press 1 for pythagorean therom']
-print ('math calculator 0.1')
-print ("Press 0 to search for what you want :)")
-print ('Press 1 for pythagorean therom')
-problemselection = int(input())
-print ("Your selection is:",problemselection)
-if problemselection == 0:
-    search = input("Enter the text you want to search: ")
-elif problemselection == 1:
-    print ("Which side do you want to calculate?")
-    print ("Press 1 for Hypotenuse, Press 2 for the adjacent/opposite side")
-    sideselection = int(input())
-    pythagoreantherom (sideselection)
-        
+def mainprogram():  
+    list = ['Press 1 for pythagorean therom']
+    print ('math calculator 0.1')
+    print ('Press 0 to exit')
+    print ("Press 1 to search for what you want :)")
+    print ('Press 2 for pythagorean therom')
+    problemselection = int(input("Type your selection: "))
+    print ("Your selection is:",problemselection)
+    if problemselection == 0:
+        exit
+    elif problemselection == 1:
+        search = input("Enter the text you want to search: ")
+    elif problemselection == 2:
+        print ("Which side do you want to calculate?")
+        print ("Press 1 for Hypotenuse, Press 2 for the adjacent/opposite side")
+        sideselection = int(input())
+        pythagoreantherom (sideselection)
+       
+mainprogram() 
         
         
